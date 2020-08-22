e853fd0a678b81221fff8908c30f965c93fb53d5
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-08-22 18:40:07 -0300

    criação do repositório TypeORM

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
 
@@ -10,5 +12,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
