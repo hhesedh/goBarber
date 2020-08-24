e3e2d05afa44a314ef1b6c75e03c7e9a0847b3e6
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-08-23 22:07:14 -0300

    criação de registros de usuários

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appointmentsRoutes from './appointments.routes';
+import UsersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
+routes.use('/users', UsersRouter);
 
 export default routes;
