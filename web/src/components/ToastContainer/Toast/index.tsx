c804ded6bd7996fec7e076d007b2ab5d76915020
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-11-09 02:07:15 -0300

    web: implementação de animação nos toasts

--- web/src/components/ToastContainer/Toast/index.tsx
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -10,6 +10,7 @@ import { Container } from "./styles";
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -17,7 +18,7 @@ const icons = {
   error: <FiAlertCircle size={24} />,
   success: <FiCheckCircle size={24} />,
 };
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -31,7 +32,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [message.id, removeToast]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      style={style}
+      type={message.type}
+      hasDescription={!!message.description}
+    >
       {icons[message.type || "info"]}
       <div>
         <strong>{message.title}</strong>
