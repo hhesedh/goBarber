098987377ec2d644b05d16955e26d7f698f68e02
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-11-13 22:54:22 -0300

    web: implementação de cadastro com o backend e redirecionamento para tela de dashboard ao fazer login

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Button from "../../components/Button";
+import { useAuth } from "../../hooks/auth";
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <>
+      <h1>DashBoard</h1>
+      <Button
+        onClick={(event) => {
+          event.preventDefault();
+          signOut();
+        }}
+      >
+        Deslogar
+      </Button>
+    </>
+  );
+};
 
 export default Dashboard;
