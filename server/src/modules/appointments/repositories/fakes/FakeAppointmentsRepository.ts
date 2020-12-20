bc5946ca61ee2c67c76e83a3d773b0340ddb7ffd
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-12-20 02:17:38 -0300

    implementação de teste na criação de agendamento com dois horários iguais

--- server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -3,13 +3,14 @@ import { v4 as uuid } from 'uuid';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { isEqual } from 'date-fns';
 
 class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
